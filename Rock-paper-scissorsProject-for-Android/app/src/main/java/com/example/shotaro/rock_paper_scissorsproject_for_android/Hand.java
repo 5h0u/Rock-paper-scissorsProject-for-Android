b5383c7717711d17d1b1043cab6edd61f813b0bd
山崎祥太郎 <shotaro@yamazakishoutarou-no-MacBook-Air.local>
2016-07-05 22:17:24 +0900

    出す手を決めるクラス作成

---
+++ Rock-paper-scissorsProject-for-Android/app/src/main/java/com/example/shotaro/rock_paper_scissorsproject_for_android/Hand.java
@@ -0,0 +1,24 @@
+package com.example.shotaro.rock_paper_scissorsproject_for_android;
+
+/**
+ * Created by shotaro on 2016/07/05.
+ */
+
+/* 出せる手 */
+public class Hand {
+
+    /*グー*/
+    private int Rock(){
+        return 0;
+    }
+
+    /*パー*/
+    private int paper(){
+        return 1;
+    }
+
+    /*チョキ*/
+    private int scissors(){
+        return 2;
+    }
+}
